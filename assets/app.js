f2ca66867e216e2de0b5beb9731b16345e6676bb
dimitri.makogon <dimitri.makogon@gmail.com>
2025-12-02 08:14:58 +0000

    Changes before Firebase Studio auto-run

--- assets/app.js
+++ assets/app.js
@@ -6,5 +6,7 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
+import '@craue/form-flow-bundle/js/form_flow.js';
+import '@craue/form-flow-bundle/js/form_flow_basic.js';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
